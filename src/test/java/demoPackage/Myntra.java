dc4e060dcc42eeb61dbd865023ec0e5cb22fe8de
SarkarNiladri01 <sarkar.niladri27@gmail.com>
2023-07-30 22:22:33 +0530

    [Narayana] added myntra

---
+++ src/test/java/demoPackage/Myntra.java
@@ -0,0 +1,27 @@
+package demoPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Myntra {
+
+	public static WebDriver driver;
+	  @Test
+	  public void amazon() throws InterruptedException {
+		  
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");  
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.quit();
+		
+		  
+		  
+	
+	
+	  }}
